f389a1fd4ea14db80ec244ef1334f38b669213bf
Lewis <77336630+Lmalon9@users.noreply.github.com>
2021-02-08 13:22:38 +0000

    Added User Update also ToastAndroid

--- components/account_update.js
+++ components/account_update.js
@@ -1,28 +1,95 @@
 import 'react-native-gesture-handler';
 import React, { Component, useEffect, useState } from 'react';
-import { Text, View, TextInput, Button, StyleSheet, Alert, TouchableOpacity, NativeModules, StatusBar, FlatList, SafeAreaView} from 'react-native';
+import { Text, ToastAndroid, View, TextInput, Button, StyleSheet, Alert, TouchableOpacity, NativeModules, StatusBar, FlatList, SafeAreaView} from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
+
 function AccountUpdate(){
-    const [first_name, setFirstName] = useState('Test'); // initialize state
+    const [first_name, setFirstName] = useState(''); // initialize state
     const [last_name, setLastName] = useState(''); // initialize state
     const [email, setEmail] = useState(''); // initialize state
     const [password, setPassword] = useState(''); // initialize state
     const navigation = useNavigation();
 
     useEffect(() => {
-         
-           
-         
+      navigation.addListener('focus', () => {
+        getUserInfo();
+      }
+      )
+      async function getUserInfo(){
+      var idlog = JSON.parse(await AsyncStorage.getItem('@session_token')).id;
+      var tokenlog = JSON.parse(await AsyncStorage.getItem('@session_token')).token;
+      fetch(`http://10.0.2.2:3333/api/1.0.0/user/${idlog}`,
+      {
+        method: 'get',
+        headers: {
+          "Content-Type": "application/json",
+          "X-Authorization": tokenlog,
+        },
+      })
+      .then ((res) => {
+        if (res.status === 200)
+        {
+          return res.json();
         }
-    );
+        else{
+          throw 'failed';
+        };
+  
+      })
+      .then ( (data) => {
+        console.log(data);
+        setFirstName(data.first_name);
+        setLastName(data.last_name);
+        setEmail(data.email);
+  
+      })
+      .catch( (message) => { console.log("ERROR" + message)})
+    }
+    });
 
     const send_update = async () => {
         var idlog = JSON.parse(await AsyncStorage.getItem('@session_token')).id;
+        var tokenlog = JSON.parse(await AsyncStorage.getItem('@session_token')).token;
         console.log(idlog)
+        fetch(`http://10.0.2.2:3333/api/1.0.0/user/${idlog}`,
+        {
+          method: 'patch',
+          headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": tokenlog,
+          
+          },
+          body: JSON.stringify( {
+            first_name: first_name,
+            last_name:last_name,
+            email: email,
+            password: password,
+          }),
+      })
 
+      .then ((res) => {
+        if (res.status == 200)
+        {
+          ToastAndroid.showWithGravity("Update Successful", ToastAndroid.SHORT, ToastAndroid.CENTER)
+          navigation.navigate("Account")
+          return;
+          
+        }
+        else{
+          throw 'failed';
+        };
+  
+      })
+      // .then (async (data) => {
+  
+      //   console.log(data);
+      //   navigation.navigate("Account")
+  
+      // })
+      .catch( (message) => { console.log("ERROR" + message)})
     }
 
     return (
@@ -33,18 +100,18 @@ function AccountUpdate(){
             onChangeText = {text => setFirstName(text)}
             />
             <TextInput
+            value = {last_name}
             onChangeText = {text => setLastName(text)}
             />
-            <TextInput>
-            {email}
-            </TextInput>
-            <TextInput>
-            {password}
-            </TextInput>
+            <TextInput
+            value = {email}
+            onChangeText = {text => setEmail(text)}            />
+            <TextInput
+            onChangeText = {text => setPassword(text)}            />
             <TouchableOpacity style={styles.button}>
               <Button
               title="Update" 
-              //onPress={}
+              onPress={send_update}
               />
             </TouchableOpacity>
         </View>
