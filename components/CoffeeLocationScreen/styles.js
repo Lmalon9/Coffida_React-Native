0685d4fedbc48c4d8f7c0810198fab3c4e03e8a4
Lewis <77336630+Lmalon9@users.noreply.github.com>
2021-02-24 12:05:33 +0000

    Code Cleanup and moved styles to their own page

---
+++ components/CoffeeLocationScreen/styles.js
@@ -0,0 +1,29 @@
+import 'react-native-gesture-handler';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+  
+    },
+    button:{
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#151A30',
+        borderColor: '#151A30',
+  
+    },
+        buttonLike:{
+        alignContent: 'center',
+        alignItems: 'center',
+        width: 50 ,
+        justifyContent: 'center',
+        color: '#ff0000'
+        //backgroundColor: '#151A30',
+        //borderColor: '#151A30',
+  
+    },
+  });   
+  
+export default styles
